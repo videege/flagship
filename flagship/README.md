b5bb6865952f762d4c84fcb12bcd5d4105bccf74
Team UF <teamuf@Teams-MacBook-Air.local>
2019-09-13 09:57:44 -0600

    firebase init

--- flagship/README.md
+++ flagship/README.md
@@ -1,4 +1,4 @@
-# ClientApp
+# flagship
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.3.
 
--- flagship/src/app/app.component.spec.ts
+++ flagship/src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'ClientApp'`, async(() => {
+  it(`should have as title 'flagship'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ClientApp');
+    expect(app.title).toEqual('flagship');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ClientApp!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to flagship!');
   }));
 });
