2d9f9cc0228d98ffa0d226f2340c82d971fd9784
Team UF <teamuf@Teams-MacBook-Air.local>
2019-12-01 20:10:43 -0700

    fix: apply fleet wide effects properly.

--- flagship/src/app/domain/statistics/factories/attackEffectFactory.ts
+++ flagship/src/app/domain/statistics/factories/attackEffectFactory.ts
@@ -19,6 +19,8 @@ export enum ModificationClass {
 
 export interface IEffectData {
     id: number;
+    appliesToFleet: boolean;
+    appliesOnlyToFleet?: boolean;
     factory(): IDieModification;
 }
 
@@ -26,9 +28,13 @@ export class AttackEffectFactory {
     shipEffectData: IEffectData[];
     squadronEffectData: IEffectData[];
 
-    hasShipEffect(upgradeId: number): boolean {
-        let idx = this.shipEffectData.findIndex(x => x.id === upgradeId);
-        return idx >= 0;
+    hasShipEffect(upgradeId: number, mustBeFleetWide: boolean = false): boolean {
+        let effect = this.shipEffectData.find(x => x.id === upgradeId);
+        if (effect && mustBeFleetWide && !effect.appliesToFleet)
+            return false;
+        else if (effect && !mustBeFleetWide && effect.appliesOnlyToFleet)
+            return false;
+        return !!effect;
     }
 
     hasSquadronEffect(squadronId: number): boolean {
@@ -54,7 +60,15 @@ export class AttackEffectFactory {
             return self.indexOf(value) === index;
         }).filter(x => this.hasSquadronEffect(x)).map(x => this.instantiateEffect(x, false));
 
-        let finalMods = concentrate.concat([].concat(...shipMods)).concat([].concat(...squadronMods));
+        let otherShipMods: IDieModification[] = [];
+        for (let otherShip of ship.fleet.ships.filter(x => x.uid !== ship.uid)) {
+            otherShipMods = otherShipMods.concat([], ...otherShip.upgradeSlots.filter(x => x.isEnabled && x.isFilled()).map(x => x.upgrade.id)
+                .filter(x => this.hasShipEffect(x, true)).map(x => this.instantiateEffect(x, true)));
+        }
+        let finalMods = concentrate
+            .concat([].concat(...shipMods))
+            .concat(otherShipMods)
+            .concat([].concat(...squadronMods));
         //todo: search fleet for upgrades that affect multiple ships (e.g., Darth Vader)
         finalMods = finalMods.sort((a, b) => {
             if (a.order > b.order) return 1;
@@ -83,108 +97,110 @@ export class AttackEffectFactory {
         this.concentrateFireTokenReroll.enabled = false;
         this.squadronEffectData = [
             {
-                id: 7, factory: () => {
+                id: 7, appliesToFleet: true, factory: () => {
                     return new GenericModification('Captain Jonus', this.orders.modification,
-                    true, DieType.Any, [],
-                    [FaceRestriction.Accuracy], 0, [], [DieType.Red, DieType.Blue, DieType.Black], FaceRestriction.Accuracy)
+                        true, DieType.Any, [],
+                        [FaceRestriction.Accuracy], 0, [], [DieType.Red, DieType.Blue, DieType.Black], FaceRestriction.Accuracy)
                 }
             },
             {
-                id: 116, factory: () => {
+                id: 116, appliesToFleet: true, factory: () => {
                     return new GenericModification('Malee Hurra', this.orders.modification,
-                    true, DieType.Any, [],
-                    [FaceRestriction.Crit], 1, [DieType.Red, DieType.Blue, DieType.Black], 
-                    [DieType.Black, DieType.Red, DieType.Blue], FaceRestriction.Crit)
+                        true, DieType.Any, [],
+                        [FaceRestriction.Crit], 1, [DieType.Red, DieType.Blue, DieType.Black],
+                        [DieType.Black, DieType.Red, DieType.Blue], FaceRestriction.Crit)
                 }
             },
         ];
         this.shipEffectData = [
             // armament additions
             {
-                id: 12001, factory: () => {
+                id: 12001, appliesToFleet: false, factory: () => {
                     return new AdditionModification('Enhanced Armament', 1,
                         false, this.orders.armamentAddition, AdditionRestriction.Red, FiringArc.Left | FiringArc.Right)
                 }
             },
             {
-                id: 12005, factory: () => {
+                id: 12005, appliesToFleet: false, factory: () => {
                     return new AdditionModification('Quad Battery Turrets', 1,
                         true, this.orders.addition, AdditionRestriction.Blue)
                 }
             },
             {
-                id: 12007, factory: () => {
+                id: 12007, appliesToFleet: false, factory: () => {
                     return new AdditionModification('Slaved Turrets', 1,
                         true, this.orders.addition, AdditionRestriction.Red)
                 }
             },
             {
-                id: 12008, factory: () => {
+                id: 12008, appliesToFleet: false, factory: () => {
                     return new AdditionModification('Spinal Armament', 1,
                         false, this.orders.armamentAddition, AdditionRestriction.Red, FiringArc.Front | FiringArc.Rear)
                 }
             },
             // replacement
             {
-                id: 1502, factory: () => {
+                id: 1502, appliesToFleet: true, factory: () => {
                     return new ReplacementModification('Commander Sato', 2, false, this.orders.replacement)
                 }
             },
             // pool additions
             {
-                id: 2022, factory: () => {
+                id: 2022, appliesToFleet: false, factory: () => {
                     return new AdditionModification('Ravager (Concentrate Token)', 1,
                         true, this.orders.existingAddition, AdditionRestriction.Existing)
                 }
             },
             {
-                id: 1012, factory: () => {
+                id: 1012, appliesToFleet: true, factory: () => {
                     return new AdditionModification('General Romodi', 1,
                         true, this.orders.addition, AdditionRestriction.Red)
                 }
             },
             {
-                id: 1500, factory: () => {
+                id: 1500, appliesToFleet: true, factory: () => {
                     return new AdditionModification('Admiral Ackbar', 2,
                         true, this.orders.addition, AdditionRestriction.Red, FiringArc.Left | FiringArc.Right)
                 }
             },
             {
-                id: 2516, factory: () => {
+                id: 2516, appliesToFleet: false, factory: () => {
                     return new AdditionModification('Defiance', 1,
                         true, this.orders.addition, AdditionRestriction.Any)
                 }
             },
             {
-                id: 7003, factory: () => {
+                id: 7003, appliesToFleet: false, factory: () => {
                     return new AdditionModification('External Racks', 2,
                         true, this.orders.addition, AdditionRestriction.Black, null,
                         Range.Close)
                 }
             },
             {
-                id: 12006, factory: () => {
+                id: 12006, appliesToFleet: false, factory: () => {
                     return new QuadTurbolaserCannonsModification(this.orders.modification + 1)
                 }
             },
             //TODO; devastator (variable # up to 5)
             // Rerolls
-            { id: 3001, factory: () => { return new LeadingShotsModification(RerollStrategy.Blanks, this.orders.expensiveReroll); } },
-            { id: 4006, factory: () => { return new VeteranGunnersModification(RerollStrategy.Blanks, this.orders.expensiveReroll); } },
+            { id: 3001, appliesToFleet: false, 
+                factory: () => { return new LeadingShotsModification(RerollStrategy.Blanks, this.orders.expensiveReroll); } },
+            { id: 4006, appliesToFleet: false, 
+                factory: () => { return new VeteranGunnersModification(RerollStrategy.Blanks, this.orders.expensiveReroll); } },
             {
-                id: 1009, factory: () => {
+                id: 1009, appliesToFleet: true, factory: () => {
                     return new GenericRerollModification(RerollStrategy.Blanks,
                         'Darth Vader (Commander)', this.orders.reroll, 0, DieType.Any);
                 }
             },
             {
-                id: 2510, factory: () => {
+                id: 2510, appliesToFleet: false, factory: () => {
                     return new GenericRerollModification(RerollStrategy.Blanks,
                         'Task Force Organa', this.orders.reroll, 2, DieType.Any);
                 }
             },
             {
-                id: 4001, factory: () => {
+                id: 4001, appliesToFleet: false, factory: () => {
                     return new GenericRerollModification(RerollStrategy.Blanks,
                         'Ordnance Experts', this.orders.reroll, 0, DieType.Black);
                 }
@@ -192,76 +208,76 @@ export class AttackEffectFactory {
             //todo: caitken and shollan (variable reroll of one color)
             //todo: dual turbolaser turrets (red die replacement fresh roll)
             {
-                id: 10212, factory: () => {
+                id: 10212, appliesToFleet: false, factory: () => {
                     return new GenericRerollModification(RerollStrategy.Blanks,
                         'Director Krennic (Dial)', this.orders.reroll, 0, DieType.Red,
                         Range.Long);
                 }
             },
             {
-                id: 10212, factory: () => {
+                id: 10212, appliesToFleet: false, factory: () => {
                     return new GenericRerollModification(RerollStrategy.Blanks,
                         'Director Krennic (Token)', this.orders.reroll, 2, DieType.Red,
                         Range.Long);
                 }
             },
             {
-                id: 12004, factory: () => {
+                id: 12004, appliesToFleet: false, factory: () => {
                     return new GenericRerollModification(RerollStrategy.Blanks,
                         'Linked Turbolaser Turrets', this.orders.reroll, 1, DieType.Red);
                 }
             },
             // Modifications
             {
-                id: 12002, factory: () => {
+                id: 12002, appliesToFleet: false, factory: () => {
                     return new GenericModification('H9 Turbolasers', this.orders.modification,
-                    true, DieType.Any, [FaceRestriction.Hit, FaceRestriction.Crit],
-                    [FaceRestriction.Accuracy], 0, [], [DieType.Red, DieType.Blue, DieType.Black], FaceRestriction.Accuracy)
+                        true, DieType.Any, [FaceRestriction.Hit, FaceRestriction.Crit],
+                        [FaceRestriction.Accuracy], 0, [], [DieType.Red, DieType.Blue, DieType.Black], FaceRestriction.Accuracy)
                 }
             },
             {
-                id: 12009, factory: () => {
+                id: 12009, appliesToFleet: false, factory: () => {
                     return new GenericModification('Turbolaser Reroute Circuits', this.orders.modification,
-                    true, DieType.Red, [],
-                    [FaceRestriction.DoubleHit, FaceRestriction.Crit], 0, [], 
-                    [DieType.Red, DieType.Blue, DieType.Black], FaceRestriction.DoubleHit)
+                        true, DieType.Red, [],
+                        [FaceRestriction.DoubleHit, FaceRestriction.Crit], 0, [],
+                        [DieType.Red, DieType.Blue, DieType.Black], FaceRestriction.DoubleHit)
                 }
             },
             {
-                id: 1007, factory: () => {
+                id: 1007, appliesToFleet: true, factory: () => {
                     return new GenericModification('Admiral Screed', this.orders.modification,
-                    true, DieType.Any, [],
-                    [FaceRestriction.Crit], 1, [DieType.Red, DieType.Black, DieType.Blue], 
-                    [DieType.Black, DieType.Red, DieType.Blue], FaceRestriction.Crit)
+                        true, DieType.Any, [],
+                        [FaceRestriction.Crit], 1, [DieType.Red, DieType.Black, DieType.Blue],
+                        [DieType.Black, DieType.Red, DieType.Blue], FaceRestriction.Crit)
                 }
             },
             {
-                id: 4005, factory: () => {
+                id: 4005, appliesToFleet: false, factory: () => {
                     return new GenericModification('Sensor Team', this.orders.modification,
-                    true, DieType.Any, [],
-                    [FaceRestriction.Accuracy], 1, [DieType.Red, DieType.Black, DieType.Blue], 
-                    [DieType.Red, DieType.Blue, DieType.Black], FaceRestriction.Accuracy)
+                        true, DieType.Any, [],
+                        [FaceRestriction.Accuracy], 1, [DieType.Red, DieType.Black, DieType.Blue],
+                        [DieType.Red, DieType.Blue, DieType.Black], FaceRestriction.Accuracy)
                 }
             },
             {
-                id: 2517, factory: () => {
+                id: 2517, appliesToFleet: true, appliesOnlyToFleet: true, factory: () => {
                     return new GenericModification('Home One', this.orders.modification,
-                    true, DieType.Any, [],
-                    [FaceRestriction.Accuracy], 0, [], [DieType.Red, DieType.Blue, DieType.Black], FaceRestriction.Accuracy)
+                        true, DieType.Any, [],
+                        [FaceRestriction.Accuracy], 0, [], [DieType.Red, DieType.Blue, DieType.Black], FaceRestriction.Accuracy)
                 }
             },
             {
-                id: 4007, factory: () => {
+                id: 4007, appliesToFleet: false, factory: () => {
                     return new GenericModification('Weapons Battery Techs', this.orders.modification,
-                    true, DieType.Any, [FaceRestriction.Accuracy],
-                    [FaceRestriction.Crit], 0, [], [DieType.Blue, DieType.Red, DieType.Black], FaceRestriction.Crit)
+                        true, DieType.Any, [FaceRestriction.Accuracy],
+                        [FaceRestriction.Crit], 0, [], [DieType.Blue, DieType.Red, DieType.Black], FaceRestriction.Crit)
                 }
             },
             {
-                id: 8002, factory: () => {
+                id: 8002, appliesToFleet: true, factory: () => {
                     return new GenericModification('Intensify Firepower!', this.orders.modification,
-                    true, DieType.Any, [],
-                    [FaceRestriction.Hit], 0, [], [DieType.Red, DieType.Black, DieType.Blue], FaceRestriction.Hit)
+                        true, DieType.Any, [],
+                        [FaceRestriction.Hit], 0, [], [DieType.Red, DieType.Black, DieType.Blue], FaceRestriction.Hit)
                 }
             },
         ];
